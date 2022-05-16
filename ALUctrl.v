d2aa377f7327601d9249bebbc3d7a35b61d1392c
nishata24 <94148211+nishata24@users.noreply.github.com>
2022-05-15 20:33:52 -0400

    Update ALUctrl.v

--- ALUctrl.v
+++ ALUctrl.v
@@ -2,22 +2,21 @@
 
 module ALUctrl(zero, result, slt_reg, ALUctrlbits, data1, data2,clk);
 
-//-------------Input Ports-----------------------------
-input wire [2:0] ALUctrlbits;	//control bit from ALU control unit
-input wire signed [7:0] data1;			//8 bits of data1
-input wire signed [7:0] data2;			//8 bits of data2
+//Input Ports
+	input wire [2:0] ALUctrlbits;	//ALU control unit control bits
+input wire signed [7:0] data1;			
+input wire signed [7:0] data2;			
 input wire clk;
 //input wire slt0_reg, slt1_reg;	//check slt0_reg, slt1_reg from register file
 
-//-------------Output Ports----------------------------
-output reg zero; 			//Gives 1 for jumping
-output reg [7:0] result; 	//8 bits of results
-output reg slt_reg;			//to be fed into register file to write on slt0_reg or slt1_reg
+//Output Ports
+output reg zero; 			//1 for jumping
+output reg [7:0] result; 	
+output reg slt_reg;			//feed into register file to write on slt0_reg or slt1_reg
 
 reg [7:0] save_msb;
 
-//------------------Instructions-----------------------
-
+//Instructions
 // 001: add/addi
 // 010: nand
 // 011: comparison
@@ -33,7 +32,7 @@ end
 
 always @*
 	begin
-		//general ALU computation for R types; result
+		//ALU computation for R types; result
 		result = 0;
 		zero = 0;
 		if (ALUctrlbits == 3'b001) begin	//add
@@ -54,7 +53,7 @@ always @*
 			result = save_msb + (data1 >> 1);
 		end 
 		
-		//general calculation for I types; zero
+		//calculation for I types; zero
 		if (ALUctrlbits == 3'b011) begin	//slt_0 and slt_1
 			if (data1 > data2) begin
 				result = 8'b11111111;
@@ -72,7 +71,7 @@ always @*
 		end
 
 
-		//effective memory address calculation
+		//memory address calculation
 		if (ALUctrlbits == 3'b111) begin		//lw or sw
 			result = data1 + data2;				//reg($sp) + imm offset = address in memory
 		end
